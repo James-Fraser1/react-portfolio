90c0323b8afac92b031c96b0eb4c57db26c5c5cf
James-Fraser1 <jdfraser2022@gmail.com>
2022-10-15 19:19:32 -0400

    refactoring

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     {/* <HashRouter> */}
-    <BrowserRouter basename='/React'>
+    <BrowserRouter basename='/react-portfolio'>
       <App />
     </BrowserRouter>
     {/* </HashRouter> */}
