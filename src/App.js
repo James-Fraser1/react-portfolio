85ffdc72baf92a09abb65f884020c96c7ae0abea
James-Fraser1 <jdfraser2022@gmail.com>
2022-10-15 12:18:19 -0400

    refactor

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Routes, Route, Router } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
 
@@ -10,11 +10,11 @@ function App() {
   return (
     <div className='background'>
       <Header />
-      <Router>
+      <Routes>
         <Route path='/' element={<About />} />
         <Route path='/contact' element={<Contact />} />
         <Route path='/projects' element={<Projects />} />
-      </Router>
+      </Routes>
       <Footer></Footer>
     </div>
 
